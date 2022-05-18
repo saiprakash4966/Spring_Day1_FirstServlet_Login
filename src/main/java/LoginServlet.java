e6d979911553a94a285552ed4146a3ccca5f6179
Your Name <you@example.com>
2022-05-18 12:27:02 +0530

    By extending servlet updating valid user name

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,34 +7,49 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
         urlPatterns = {"/LoginServlet"},
-        initParams = {
-                @WebInitParam(name = "user", value = "Sai"),
-                @WebInitParam(name = "password", value = "4966")
-        }
-)
-public class LoginServlet extends HttpServlet {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // get request params for user and password
-        String user = req.getParameter("user");
-        String pwd = req.getParameter("pwd");
-        // get servlet config init params
-        String userName = getServletConfig().getInitParameter("user");
+        // Setting a couple of initial parameter for the servlet
+        initParams = { @WebInitParam(name = "user", value = "Sai"),
+                @WebInitParam(name = "password", value = "Prakash@123") })
+
+public class LoginServlet extends HttpServlet {
+    // Define the post method for when api calls the endpoint
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // The user and password are taken from the request
+        String userInput = request.getParameter("user");
+        String pwdInput = request.getParameter("pwd");
+
+        // The user and password are retrieved from the initial params of the servlet
+        String user = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if (userName.equals(user) && password.equals(pwd)) {
-            req.setAttribute("user", user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
-        } else {
-            RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter printWriter = resp.getWriter();
-            printWriter.println("<font color=red>Either user name or password is wrong</font>");
-            requestDispatcher.include(req, resp);
+
+        Pattern pattern = Pattern.compile("^([A-Z][a-zA-Z]{2,}[ ]?)+$");
+        Matcher matcher = pattern.matcher(userInput);
+        if(!matcher.matches()) {
+            PrintWriter out = response.getWriter();
+            out.println("<font color = red> Incorrect UserId. Please try again!!<font>");
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            rd.include(request, response);
+            return;
+        }
+
+        if (user.equals(userInput) && password.equals(pwdInput)) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("/LoginSuccess.jsp").forward(request, response);
+        }
+
+        else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color = red> Incorrect UserId or Password. Please try again!!<font>");
+            rd.include(request, response);
         }
     }
 }
